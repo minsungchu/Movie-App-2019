705db68c03911fe639cbd0edc1f3a48e172ef9a5
mschu <chuminsung@gmail.com>
2020-08-20 07:49:27 +0900

    Add Food Component with JSX and Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+function Food({ favourite }) {
+  return <h1>I like {favourite}</h1>
+}
+
 function App() {
   return (
     <div className="App">
-
+      <h1>Hello React JS.</h1>
+      <Food favourite="kimchi" />
+      <Food favourite="noodle" />
+      <Food favourite="rice" />
+      <Food favourite="egg" />
     </div>
   );
 }
